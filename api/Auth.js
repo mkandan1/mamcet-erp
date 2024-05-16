96799773192aae3ce8d2b868376f6522dd317bed
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2024-05-16 19:42:31 +0530

    add: auth state management

--- api/Auth.js
+++ api/Auth.js
@@ -1,4 +1,22 @@
+import { logOut } from "../redux/actions/authActions";
 import { API } from "./API";
+
+export class Authorization {
+    static async onAuthState(dispatch) {
+        try {
+            const userAuthStateResult = await API.getRequest('/auth/user-state')
+            if (!userAuthStateResult.success) {
+                return dispatch(logOut())
+            }
+            console.log("user logged in")
+            dispatch(setLoggedIn({ user: userAuthStateResult.user }))
+            return
+        }
+        catch(err){
+            console.error(err.message)
+        }
+    }
+}
 
 export class Auth {
     static async signInWithEmailAndPassword(email, password) {
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Login } from './screens/Login'
+import { useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { Authorization } from '../api/Auth';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [isCheckingAuthCompleted, setIsCheckingAuthCompleted] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    localStorage.getItem('mamcet_auth') ? setIsLoggedIn(true) : setIsLoggedIn(false);
+
+    Authorization.onAuthState(dispatch)
+      .then(status => {
+        setIsLoggedIn(true);
+      })
+      .catch(err => {
+        localStorage.removeItem('mamcet_auth')
+        setIsLoggedIn(false);
+      })
+      .finally(() => {
+        setIsCheckingAuthCompleted(true)
+      })
+  }, [])
+
+  if (!isCheckingAuthCompleted) {
+    return
+  }
 
   return (
     <Router>
